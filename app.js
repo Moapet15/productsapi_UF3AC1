e0132881167a1ee87e3278c51279e57ecfe93931
Moapet15 <franquetandreu@gmail.com>
2024-06-05 19:07:13 +0200

    Afegim Slugfy

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const express = require('express')
-const products_routes = require('./routes/products.js')
+const express = require('express');
+const products_routes = require('./routes/products.js');
+const slugify = require('slugify');
 
-//Server instantiation
-const app = express()
+// Server instantiation
+const app = express();
 
-//Server configuration: template engine
+// Server configuration: template engine
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('/views'));
 
-//Midleware
-app.use(express.json())
-app.use('/', products_routes)
+// Midleware
+app.use(express.json());
+app.use('/', products_routes);
 
-//Server startup
+// Missatge de benvinguda original
+const welcomeMessage = "Benvingut al servidor Express!";
+
+// Missatge de benvinguda slugitzat amb asteriscs
+const slugifiedMessage = slugify(welcomeMessage, { replacement: '*' });
+
+// Ruta principal per mostrar el missatge de benvinguda slugitzat
+app.get('/welcome', (req, res) => {
+  res.send(slugifiedMessage);
+});
+
+// Server startup
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
-})
+  console.log('server is listening on port 5000');
+});
